9b613f0317b54486ea867e620c94cfe6414cf4ed
toshko89 <todor_petkov@mail.bg>
2021-10-04 22:59:39 +0300

    new details page

--- src/controllers/attachController.js
+++ src/controllers/attachController.js
@@ -2,8 +2,8 @@ const attachController = require('express').Router();
 const accessoryService = require('./../services/accessoryService.js');
 
 
-attachController.get('/accessory',(req,res)=>{
-    res.render('attach-accessory');
+attachController.get('/accessory/:Id',(req,res)=>{
+    res.render('accessory/attach-accessory');
 })
 
 module.exports = attachController;
