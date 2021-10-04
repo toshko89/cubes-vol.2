585dab7f60177aa461a90a6d8ff811a7d51eaddf
toshko89 <todor_petkov@mail.bg>
2021-10-04 22:07:31 +0300

    update accessory service

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -4,8 +4,19 @@ async function add(name,description,imageUrl){
     return Accessory.create({name,description,imageUrl});
 }
 
+async function getAccessory(accessoryId){
+    let result = await Accessory.findById(accessoryId).lean();
+    return result;
+}
+
+async function getAllAccessories(){
+    return Accessory.find({}).lean();
+}
+
 const accessoryService = {
     add,
+    getAccessory,
+    getAllAccessories,
 }
 
 module.exports = accessoryService;
