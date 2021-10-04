4cb4d8bbfe8edf86719143f634cdd8a8cffbcdc0
toshko89 <todor_petkov@mail.bg>
2021-10-05 00:09:12 +0300

    update for attachAccessoryController.js

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -10,7 +10,7 @@ async function getAccessory(accessoryId){
 }
 
 async function getAllAccessories(){
-    return Accessory.find({});
+    return Accessory.find({}).lean();
 }
 
 const accessoryService = {
