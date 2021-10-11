6e278d2dc2c84bd39f415c61fcdbd26e1e6eb11b
toshko89 <todor_petkov@mail.bg>
2021-10-11 21:45:59 +0300

    create jwt token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
+const authRouter = require('express').Router();
 const authService = require('../services/authService.js');
 
-const authRouter = require('express').Router();
 
 authRouter.get('/register', (req, res) => {
     res.render('auth-pages/register');
@@ -8,16 +8,16 @@ authRouter.get('/register', (req, res) => {
 
 authRouter.post('/register', async (req, res) => {
     let { username, password, repeatPassword } = req.body;
-    if (username == '' || password == '' || repeatPassword == '') {
-        throw new Error('All field are required!');
-    }
-
-    if (password !== repeatPassword) {
-        throw new Error('Please re-enter your password');
-    }
-
     try {
+        if (username == '' || password == '' || repeatPassword == '') {
+            throw new Error('All field are required!');
+        }
+
+        if (password !== repeatPassword) {
+            throw new Error('Please re-enter your password');
+        }
         await authService.registerUser(username, password);
+        res.redirect('/');
     } catch (err) {
         console.log(err);
     }
@@ -28,11 +28,21 @@ authRouter.get('/login', (req, res) => {
 });
 
 authRouter.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    try{
+        if (username == '' || password == '') {
+            throw new Error('All field are required!');
+        }
+        
 
+
+    }catch(err){
+
+    }
 });
 
 authRouter.get('/logout', (req, res) => {
-
+    res.redirect('/');
 })
 
 module.exports = authRouter;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,27 @@
-const User = require('../models/User.js');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
+const config = require('../config/config.json');
 
 async function registerUser(username, password) {
     const hash = await bcrypt.hash(password, 10);
-    const user = await User.create({username, password:hash});
-    return user;
+    const user = await User.create({ username, password: hash });
+
+    const token = createToken(user);
+    
+}
+
+async function login(username, password) {
+
+}
+
+function createToken(user) {
+    const token = jwt.sign({ _id: user._id, username: user.username }, config.JWT_SECRET, { expiresIn: '2d' });
+    return token;
+}
+
+function verifyToken(token, secretKey){
+
 }
 
 const authService = {
