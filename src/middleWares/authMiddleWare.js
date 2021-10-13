e5e7f35a8474fb1aa7680a38c2326fc7ad487600
toshko89 <todor_petkov@mail.bg>
2021-10-13 14:37:47 +0300

    updated auth middleWare

--- src/middleWares/authMiddleWare.js
+++ src/middleWares/authMiddleWare.js
@@ -1,8 +1,23 @@
-const authService = require("../services/authService.js");
+const authService = require('../services/authService.js');
+const { JWT_SECRET } = require('../config/config.json');
 
-exports.auth = function (req,res,next){
-    const token = res.cookies;
-    console.log(token);
+exports.auth = function (req, res, next) {
+    const token = req.cookies['app_token'];
+
+    if (!token) {
+        return next();
+    }
+
+    const tokenVerify = authService.verifyToken(token,JWT_SECRET);
+
+    if (!tokenVerify) {
+        throw new Error('You are not authorized to view this page, please login/regiter');
+    }
+    req.user = {
+        _id:tokenVerify._id,
+        username:tokenVerify.username,
+    }
+    
     next();
 }
 
